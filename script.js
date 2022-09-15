12fd0ceb21c9d203976dbc476eaa8c077bab9eaf
Christian <christiand0393@gmail.com>
2022-09-15 00:06:59 -0500

    avance

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ window.addEventListener("scroll", function(){
     var header = this.document.querySelector("header");
     header.classList.toggle("bajar",this.window.scrollY>0);
 })
+
+/* proyectos */
+
+$(".sitio").click(function(){
+    $(".sitio").removeClass("active");
+    $(this).addClass("active");
+    
+ });
