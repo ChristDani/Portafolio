1a5b8f7679d1e3d9ed4ef7e1b78063f30c9a652b
Christian <christiand0393@gmail.com>
2022-09-18 14:07:53 -0500

    celular 90%

--- script.js
+++ script.js
@@ -4,10 +4,30 @@ window.addEventListener("scroll", function(){
     header.classList.toggle("bajar",this.window.scrollY>0);
 })
 
+/* menu hamburguesa */
+
+document.querySelector (".menuIcon") . addEventListener ("click", animateMenu) ;
+var linel=document.querySelector(".linea1");
+var line2=document.querySelector(".linea2");
+var line3=document.querySelector(".linea3");
+
+function animateMenu()
+{
+    linel.classList.toggle ("activeline1");
+    line2.classList.toggle ("activeline2");
+    line3.classList.toggle ("activeline3");
+}
+
 /* proyectos */
 
-// $(".sitio").click(function(){
-//     $(".sitio").removeClass("active");
-//     $(this).addClass("active");
-    
-//  });
+// document.querySelector (".menuIcon") . addEventListener ("click", animateMenu) ;
+// var linel=document.querySelector(".linea1");
+// var line2=document.querySelector(".linea2");
+// var line3=document.querySelector(".linea3");
+
+// function animateMenu()
+// {
+//     linel.classList.addEventListener ("active");
+//     line2.classList.remove ("active");
+//     line3.classList.toggle ("active");
+// }
