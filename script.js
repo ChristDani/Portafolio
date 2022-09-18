3708f1a2e8777010124b35d745df14736bb8f8c8
Christian <christiand0393@gmail.com>
2022-09-18 18:57:41 -0500

    solo falta agregar proyectos

--- script.js
+++ script.js
@@ -10,26 +10,18 @@ document.querySelector (".menuIcon") . addEventListener ("click", animateMenu) ;
 var linel=document.querySelector(".linea1");
 var line2=document.querySelector(".linea2");
 var line3=document.querySelector(".linea3");
+var menu=document.querySelector(".menu");
+var header=document.querySelector("header");
 
 function animateMenu()
 {
     linel.classList.toggle ("activeline1");
     line2.classList.toggle ("activeline2");
     line3.classList.toggle ("activeline3");
+    menu.classList.toggle ( "activemenu");
+    header.classList.toggle ( "active");
 }
 
 /* proyectos */
 
-// document.querySelector (".webs") . addEventListener ("click", cambiarOp) ;
-var opcion=document.querySelector(".sitio").addEventListener("click", cambiarOp) ;
 
-function cambiarOp()
-{
-    $(opcion).removeClass("active");
-    $(this).addClass("active");
-}
-
-// $(".sitio").click(function(){
-//     $(".sitio").removeClass("active");
-//     $(this).addClass("active");
-//  });
